146042b5894519a954ad5949b68ab5861217ef88
Caitlin Swickard <caitlin6029@gmail.com>
2024-02-15 15:44:12 -0700

    handel form submission

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,17 +1,47 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+// interface to define shape/fields of form
+interface TodoForm {
+  title: string;
+  dueDate: string;
+  description: string;
+}
 
 const NewTodoPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TodoForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/todos", data);
+        router.push("/todos");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <TextField.Input placeholder="Due Date: mm/dd/yy" />
-      <TextArea placeholder="Description" />
+      <TextField.Input
+        placeholder="Due Date: MM/DD/YY"
+        {...register("dueDate")}
+      />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Todo</Button>
-    </div>
+    </form>
   );
 };
 
